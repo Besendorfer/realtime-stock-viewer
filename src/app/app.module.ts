b16046a945858dd3feb3959a19ceca800130043e
Teancum Besendorfer <teancum.besendorfer@outlook.com>
2018-07-17 21:46:23 -0700

    Initialize the Angular application and set up a simple structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TableComponent } from './main/table/table.component';
+import { CompanyComponent } from './main/company/company.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TableComponent,
+    CompanyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
